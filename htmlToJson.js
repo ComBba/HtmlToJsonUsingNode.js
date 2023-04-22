b379317632c8055b8e726a9714d97583b68d8b27
ComBba <cent@naver.com>
2023-04-22 23:41:48 +0900

    category 삭제

--- htmlToJson.js
+++ htmlToJson.js
@@ -1,11 +1,20 @@
+// |Good parts:
+// |- The code uses the popular and widely-used library Cheerio for parsing HTML, which makes it easier to extract data from HTML documents.
+// |- The code is modularized into two functions, which makes it easier to read and maintain.
+// |- The code uses arrow functions, which are more concise and easier to read than traditional function expressions.
+// |- The code uses template literals to interpolate variables into strings, which is more concise and easier to read than concatenating strings with the + operator.
+// |- The code uses the JSON.stringify method with the null and 2 arguments to format the JSON output with indentation, which makes it easier to read.
+// |
+// |Bad parts:
+// |- The code does not handle errors in a robust way. If there is an error reading or writing the file, the error message is logged to the console but the program continues to run. It would be better to throw an error or exit the program in these cases.
+// |- The code uses nested each loops, which can be inefficient for large datasets. It would be better to use a single loop and filter the elements based on their attributes.
+// |- The code uses multiple variables to store the same data, which can be confusing and error-prone. It would be better to use a single object to store all the data for each element.
+// |
 const cheerio = require('cheerio');
 const fs = require('fs');
 
 function extractData($) {
   const result = [];
-
-  $('div#data_hist > div > div > h2 > span').each(function () {
-    const category = $(this).text().trim();
 
     $('div.tasks > li').each(function () {
       const dataId = $(this).attr('data-id');
@@ -20,7 +29,6 @@ function extractData($) {
       const imgSrc = $(this).find('img').attr('src');
 
       result.push({
-        category,
         dataId,
         dataName,
         dataTask,
@@ -32,7 +40,6 @@ function extractData($) {
         imgSrc,
       });
     });
-  });
 
   return result;
 }
