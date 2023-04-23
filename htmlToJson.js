29379339593a71461b96cee7cfe23b766c26bf1b
ComBba <cent@naver.com>
2023-04-23 21:39:09 +0900

    프로그램 종료시간 표시

--- htmlToJson.js
+++ htmlToJson.js
@@ -25,7 +25,19 @@ function extractData($) {
   return result;
 }
 
+function msToTime(duration) {
+  const milliseconds = parseInt((duration % 1000));
+  const seconds = parseInt((duration / 1000) % 60);
+  const minutes = parseInt((duration / (1000 * 60)) % 60);
+  const hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+
+  return `${hours}시간 ${minutes}분 ${seconds}초 ${milliseconds}ms`;
+}
+
 async function fetchAndConvertHtmlToJson(url, outputFile) {
+  const startTime = new Date();
+  console.log(`프로그램 시작 시간: ${startTime.toISOString()} (${startTime.getTime()}ms)`);
+
   try {
     const response = await axios.get(url);
     const html = response.data;
@@ -37,6 +49,10 @@ async function fetchAndConvertHtmlToJson(url, outputFile) {
         console.error(`Error writing file ${outputFile}:`, err);
         process.exit(1);
       }
+      const endTime = new Date();
+      console.log(`프로그램 종료 시간: ${endTime.toISOString()} (${endTime.getTime()}ms)`);
+      const elapsedTime = endTime.getTime() - startTime.getTime();
+      console.log(`작동하는 데 소요된 시간: ${msToTime(elapsedTime)}`);
       console.log(`Successfully fetched and converted data from ${url} to ${outputFile}`);
       console.log(`Total converted data objects: ${data.length}`);
     });
