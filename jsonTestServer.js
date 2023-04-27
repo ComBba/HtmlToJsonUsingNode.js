e7c58dc29cd8490f773ed3a3c9c5058c09336e70
ComBba <cent@naver.com>
2023-04-27 15:12:20 +0900

    검색후 페이지네이션 구현 완료. 소스정리.

--- jsonTestServer.js
+++ jsonTestServer.js
@@ -13,35 +13,37 @@ const uri = process.env.MONGODB_CONNECTION_URI;
 const dbName = process.env.MONGODB_DATABASE_NAME;
 const collectionName = process.env.MONGODB_COLLECTION_NAME;
 
+// jsonTestServer.js
 app.get('/data', async (req, res) => {
   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
   const page = parseInt(req.query.page) || 1;
+  const searchQuery = req.query.search || "";
   const itemsPerPage = 9;
-  const query = req.query.query || "";
 
   try {
     await client.connect();
     const collection = client.db(dbName).collection(collectionName);
 
-    // Modify the find() method to include the search query
-    const searchQuery = query
-      ? {
-        $or: [
-          { dataUrl: { $regex: query, $options: "i" } },
-          { dataName: { $regex: query, $options: "i" } },
-          { dataTask: { $regex: query, $options: "i" } },
-          { slug: { $regex: query, $options: "i" } },
-          { content: { $regex: query, $options: "i" } },
-          { summary: { $regex: query, $options: "i" } },
-        ],
-      }
-      : {};
+    // Use a regex pattern to match the search query
+    const pattern = new RegExp(searchQuery, "i");
 
-    const totalItems = await collection.countDocuments(searchQuery);
-    const data = await collection.find(searchQuery)
+    const query = {
+      $or: [
+        { dataId: pattern },
+        { dataName: pattern },
+        { dataTask: pattern },
+        { dataUrl: pattern },
+        { summary: pattern },
+        { useCaseText: pattern }
+      ]
+    };
+
+    const totalItems = await collection.countDocuments(searchQuery ? query : {});
+    const data = await collection.find(searchQuery ? query : {})
       .skip((page - 1) * itemsPerPage)
       .limit(itemsPerPage)
       .toArray();
+
     res.json({ data, totalItems });
   } catch (err) {
     res.status(500).send('Error fetching data from MongoDB');
--- public/app.js
+++ public/app.js
@@ -1,25 +1,29 @@
 const itemsPerPage = 9;
 let currentPage = 1;
-let paginationInitialized = false; // 추가된 부분
 
 fetchData(currentPage);
 
-let searchInput = document.getElementById("search-input");
-searchInput.addEventListener("input", () => {
-  fetchData(currentPage, searchInput.value);
-});
+const searchInput = document.getElementById("search-input");
+searchInput.addEventListener("input", onSearchInput);
 
-function fetchData(page, query = "") {
-  fetch(`/data?page=${page}&query=${encodeURIComponent(query)}`)
+// Add this function to your app.js
+function onSearchInput(event) {
+  const searchQuery = event.target.value;
+  currentPage = 1;
+  fetchData(currentPage, searchQuery);
+}
+
+function fetchData(page, searchQuery = "") {
+  fetch(`/data?page=${page}&search=${encodeURIComponent(searchQuery)}`)
     .then((response) => response.json())
     .then(({ data, totalItems }) => {
       displayGallery(data, currentPage);
-      if (!paginationInitialized) { // 추가된 부분
-        setupPagination(totalItems);
-        paginationInitialized = true;
+      if (!document.getElementById("pagination").hasChildNodes()) {
+        setupPagination(totalItems, searchQuery);
+      } else {
+        updatePagination(totalItems, searchQuery);
       }
-      updatePagination();
-      updateDataInfo(totalItems, currentPage); // 추가된 부분
+      updateDataInfo(totalItems, currentPage);
     })
     .catch((error) => {
       console.error("Error fetching JSON data:", error);
@@ -29,6 +33,8 @@ function fetchData(page, query = "") {
 function displayGallery(data, page) {
   const gallery = document.getElementById("gallery");
   gallery.innerHTML = "";
+
+  const totalItems = data.length; // Moved outside the forEach loop
 
   data.forEach((item) => {
     const galleryItem = document.createElement("div");
@@ -60,82 +66,37 @@ function displayGallery(data, page) {
     galleryItem.appendChild(img);
     galleryItem.appendChild(content);
     gallery.appendChild(galleryItem);
-
-    const totalItems = data.length;
-    displayDataInfo(totalItems, currentPage);
   });
+
+  displayDataInfo(totalItems, currentPage);
 }
 
-function setupPagination(totalItems) {
-  const pagination = document.getElementById("pagination");
+function setupPagination(totalItems, searchQuery = "") {
   const totalPages = Math.ceil(totalItems / itemsPerPage);
-
-  pagination.innerHTML = "";
-
-  for (let i = 1; i <= totalPages; i++) {
-    const pageButton = document.createElement("span");
-    pageButton.className = "page-button";
-    pageButton.innerText = i;
-    pageButton.addEventListener("click", () => {
-      currentPage = i;
-      fetchData(currentPage);
-    });
-
-    pagination.appendChild(pageButton);
-  }
+  displayPaginationButtons(1, 10, totalPages, searchQuery);
 }
 
-function createPageButton(i) {
+function updatePagination(totalItems, searchQuery = "") {
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+  const start = Math.floor((currentPage - 1) / 10) * 10 + 1;
+  const end = Math.min(start + 9, totalPages);
+
+  displayPaginationButtons(start, end, totalPages, searchQuery);
+  setActivePageButton(); // Add this line to update the active page button
+}
+
+function createPageButton(i, searchQuery) {
   const pageButton = document.createElement("span");
   pageButton.className = "page-button";
   pageButton.innerText = i;
   pageButton.addEventListener("click", () => {
     currentPage = i;
-    fetchData(currentPage); // 수정된 부분
-    updatePagination();
+    fetchData(currentPage, searchQuery);
+    setActivePageButton();
   });
 
   return pageButton;
 }
-
-function displayPaginationButtons(start, end, totalPages) {
-  const pagination = document.getElementById("pagination");
-  pagination.innerHTML = "";
-
-  const prevButton = document.createElement("span");
-  prevButton.className = "page-button";
-  prevButton.innerText = "Prev";
-  prevButton.addEventListener("click", () => {
-    if (currentPage > 1) {
-      const newStart = Math.max(start - 10, 1);
-      const newEnd = newStart + 9;
-      displayPaginationButtons(newStart, newEnd, totalPages);
-      currentPage = Math.max(currentPage - 10, 1);
-      fetchData(currentPage);
-    }
-  });
-  pagination.appendChild(prevButton);
-
-  for (let i = start; i <= end && i <= totalPages; i++) {
-    const pageButton = createPageButton(i);
-    pagination.appendChild(pageButton);
-  }
-
-  const nextButton = document.createElement("span");
-  nextButton.className = "page-button";
-  nextButton.innerText = "Next";
-  nextButton.addEventListener("click", () => {
-    if (currentPage < totalPages) {
-      const newStart = Math.min(end + 1, totalPages);
-      const newEnd = Math.min(end + 10, totalPages);
-      displayPaginationButtons(newStart, newEnd, totalPages);
-      currentPage = Math.min(currentPage + 10, totalPages);
-      fetchData(currentPage);
-    }
-  });
-  pagination.appendChild(nextButton);
-}
-
 
 function displayDataInfo(totalItems, currentPage) {
   const dataInfo = document.getElementById("data-info");
@@ -145,21 +106,55 @@ function displayDataInfo(totalItems, currentPage) {
   dataInfo.innerText = `Displaying items ${start} - ${end} of ${totalItems}`;
 }
 
-function setupPagination(totalItems, data) {
-  const totalPages = Math.ceil(totalItems / itemsPerPage);
-  displayPaginationButtons(1, 10, totalPages, data);
-  updatePagination();
-}
-
-function updatePagination() {
-  const pageButtons = document.querySelectorAll(".page-button");
-  pageButtons.forEach((button) => {
-    if (parseInt(button.innerText) === currentPage) {
+function setActivePageButton() {
+  const pageButtons = document.getElementsByClassName("page-button");
+  Array.from(pageButtons).forEach((button) => {
+    if (parseInt(button.textContent) === currentPage) {
       button.classList.add("active");
     } else {
       button.classList.remove("active");
     }
   });
+}
+
+function displayPaginationButtons(start, end, totalPages, searchQuery) {
+  const pagination = document.getElementById("pagination");
+  pagination.innerHTML = "";
+
+  const prevButton = document.createElement("span");
+  prevButton.className = "page-button";
+  prevButton.innerText = "Prev";
+  prevButton.addEventListener("click", () => {
+    if (currentPage > 1) {
+      const newStart = Math.max(start - 10, 1);
+      const newEnd = Math.min(newStart + 9, totalPages);
+      displayPaginationButtons(newStart, newEnd, totalPages, searchQuery);
+      currentPage = Math.max(currentPage - 10, 1);
+      fetchData(currentPage, searchQuery);
+    }
+  });
+  pagination.appendChild(prevButton);
+
+  for (let i = start; i <= end && i <= totalPages; i++) {
+    const pageButton = createPageButton(i, searchQuery);
+    pagination.appendChild(pageButton);
+  }
+
+  const nextButton = document.createElement("span");
+  nextButton.className = "page-button";
+  nextButton.innerText = "Next";
+  nextButton.addEventListener("click", () => {
+    if (currentPage < totalPages) {
+      const newStart = Math.min(end + 1, totalPages);
+      const newEnd = Math.min(newStart + 9, totalPages);
+      displayPaginationButtons(newStart, newEnd, totalPages, searchQuery);
+      currentPage = Math.min(currentPage + 10, totalPages);
+      fetchData(currentPage, searchQuery);
+    }
+  });
+  pagination.appendChild(nextButton);
+
+  setActivePageButton();
 }
 
 function updateDataInfo(totalItems, page) {
