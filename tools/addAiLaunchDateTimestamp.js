fed9cba5b84291ce0d2209c58f49d824129b665b
ComBba <cent@naver.com>
2023-04-28 16:45:07 +0900

    addAiLaunchDateTimestamp.js에서 addAiLaunchDateTimestamp메소드가 1초에 1만씩만 실행되도록 수정.   - DB 피로도 하향을 위한 수정.

--- tools/addAiLaunchDateTimestamp.js
+++ tools/addAiLaunchDateTimestamp.js
@@ -12,14 +12,6 @@ function convertToTimestamp(dateString) {
     return date.getTime();
 }
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-function randomInRange(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 async function addAiLaunchDateTimestamp() {
     const uri = process.env.MONGODB_CONNECTION_URI; // Replace with your MongoDB connection URI
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -28,22 +20,28 @@ async function addAiLaunchDateTimestamp() {
         const collection = client.db(process.env.MONGODB_DATABASE_NAME).collection(process.env.MONGODB_COLLECTION_NAME);
         const cursor = collection.find({});
 
-        await cursor.forEach(async (item) => {
-            await sleep(randomInRange(2000)); // 1~2초 대기
+        let lastInsertTime = 0;
+        for await (const item of cursor) {
+            const currentTime = Date.now();
+            if (currentTime - lastInsertTime < 1000) {
+                await new Promise(resolve => setTimeout(resolve, 1000 - (currentTime - lastInsertTime)));
+            }
+
             const aiLaunchDateText = item.aiLaunchDateText;
             const aiLaunchDateTimestamp = item.aiLaunchDateTimestamp;
             if (aiLaunchDateText && aiLaunchDateText.length > 0 && !aiLaunchDateTimestamp) {
                 const aiLaunchDateTimestamp = convertToTimestamp(aiLaunchDateText);
                 item.aiLaunchDateTimestamp = aiLaunchDateTimestamp;
-                await insertIntoMongoDB(item);
+                const insertResult = await insertIntoMongoDB(item);
+                if (insertResult) {
+                    lastInsertTime = Date.now();
+                }
                 console.log('[', item.dataId, ']', '[', item.dataName, ']', 'itemAdded aiLaunchDateTimestamp:', aiLaunchDateTimestamp);
             }
-        });
+        }
     } catch (err) {
         console.error('Error adding aiLaunchDateTimestamp to MongoDB:', err);
-        await sleep(randomInRange(2000));
     } finally {
-        await sleep(randomInRange(2000));
         await client.close();
     }
 }
