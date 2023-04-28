848cf6223c4b67874656a23d86e4b1f5532fa202
ComBba <cent@naver.com>
2023-04-28 17:20:54 +0900

    디버깅용 로그 추가 및 프롬프트 top3로 수정. 진행중... 아래 검토 후 추가해야함.
    const topCategory = category.split(', ')[0].split(': ')[1]; // Extract the highest-ranked category
    return `Category: ${topCategory}`;

--- tools/categorizeDocumentsWithOpenAI.js
+++ tools/categorizeDocumentsWithOpenAI.js
@@ -11,7 +11,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function categorizeDataTask(dataTask, useCaseText, summary) {
     const systemContent = "You are a helpful assistant that categorizes data.";
-    const userContent = "Please categorize the following data task into one of the following categories and respond in the format 'Category: {category_name}': Speeches, Images, Data Analysis, Videos, NLP, Chatbots, Frameworks, Education, Health, Financial Services, Logistics, Gaming, Human Resources, CRM, Contents Creation, Automation, Cybersecurity, Social Media, Environment, Smart Cities: ";
+    const userContent = "Please rank the top 3 categories from the following list for the given data task and respond in the format '1: {category_name_1}, 2: {category_name_2}, 3: {category_name_3}': Speeches, Images, Data Analysis, Videos, NLP, Chatbots, Frameworks, Education, Health, Financial Services, Logistics, Gaming, Human Resources, CRM, Contents Creation, Automation, Cybersecurity, Social Media, Environment, Smart Cities: ";
 
     const inputText = `${dataTask} ${useCaseText} ${summary}`;
 
@@ -27,35 +27,43 @@ async function categorizeDataTask(dataTask, useCaseText, summary) {
 
 async function asyncForEach(array, callback) {
     for (let index = 0; index < array.length; index++) {
-        await callback(array[index], index, array);
-        await new Promise((resolve) => setTimeout(resolve, 2000)); // 2초 지연
+        try {
+            await callback(array[index], index, array);
+        } catch (error) {
+            console.error(`Error processing item at index ${index}:`, error);
+        }
+        await new Promise((resolve) => setTimeout(resolve, 1000)); // 1초 지연
     }
 }
 
 (async function main() {
     try {
+        console.log('Categorizing data tasks started...');
         await client.connect();
+        console.log('client.connect()...');
         const db = client.db(process.env.MONGODB_DATABASE_NAME);
         const collection = db.collection(process.env.MONGODB_COLLECTION_NAME);
 
-        const documents = await collection.find({}).toArray();
-
-        await asyncForEach(documents, async (doc) => {
+        const documents = await collection.find({ category: { $exists: false } }).toArray();
+        console.log(`Found ${documents.length} documents to categorize.`);
+        await asyncForEach(documents, async (doc, index, array) => {
             const { _id, dataId, dataName, dataTask, useCaseText, summary } = doc;
 
             const categoryWithPrefix = await categorizeDataTask(dataTask, useCaseText, summary);
+            console.log("[categoryWithPrefix]", categoryWithPrefix)
             const category = categoryWithPrefix.split(': ')[1];
 
             if (category) {
                 await collection.updateOne({ _id }, { $set: { category } });
-                console.log(`[OK][${category}]${dataId}\t${dataName}\t${dataTask}\n\t${useCaseText}`);
+                console.log(`[${index + 1}/${array.length}][OK][${category}]${dataId}\t${dataName}\t${dataTask}\n\t${useCaseText}`);
             } else {
-                console.log(`[Fail]${dataId}\t${dataName}\t${dataTask}\n\t${useCaseText}`);
+                console.log(`[${index + 1}/${array.length}][Fail]${dataId}\t${dataName}\t${dataTask}\n\t${useCaseText}`);
             }
         });
     } catch (error) {
         console.error('Error categorizing data tasks:', error);
     } finally {
         await client.close();
+        console.log('Categorizing data tasks completed.');
     }
 })();
