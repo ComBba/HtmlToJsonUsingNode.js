6e4b218ea1351da308dbd272a3c6f2f91c31a36b
ComBba <cent@naver.com>
2023-05-01 09:10:51 +0900

    웹서비스에 NodeCache와 mongodb 접속 최적화 적용.

--- serverWithExpress.js
+++ serverWithExpress.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
 const { MongoClient } = require('mongodb');
+const NodeCache = require('node-cache'); // Import the node-cache package
 
 dotenv.config({ path: path.join(__dirname, '.env.local') });
 
@@ -13,17 +14,33 @@ const uri = process.env.MONGODB_CONNECTION_URI;
 const dbName = process.env.MONGODB_DATABASE_NAME;
 const collectionName = process.env.MONGODB_COLLECTION_NAME;
 
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Initialize cache with a 24-hour TTL (time to live)
+const imageCache = new NodeCache({ stdTTL: 86400 });
+
+async function getClient() {
+  if (!client.topology || !client.topology.isConnected()) {
+    try {
+      console.log('Connecting to MongoDB');
+      await client.connect();
+      console.log('Connected to MongoDB');
+    } catch (err) {
+      console.error('Error connecting to MongoDB:', err);
+      throw err;
+    }
+  }
+  return client;
+}
+
 app.get('/data', async (req, res) => {
-  console.log('GET /data request received'); // 로그 추가: 요청 받음
-  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  console.log('GET /data request received');
   const page = parseInt(req.query.page) || 1;
   const searchQuery = req.query.search || "";
   const itemsPerPage = 9;
 
   try {
-    console.log('Connecting to MongoDB'); // 로그 추가: MongoDB 연결 시도
-    await client.connect();
-    console.log('Connected to MongoDB'); // 로그 추가: MongoDB 연결 성공
+    const client = await getClient();
     const collection = client.db(dbName).collection(collectionName);
 
     // Use a regex pattern to match the search query
@@ -41,37 +58,44 @@ app.get('/data', async (req, res) => {
       ]
     };
 
-    console.log('Fetching data from MongoDB'); // 로그 추가: MongoDB에서 데이터 가져오기 시작
+    console.log('Fetching data from MongoDB');
     const totalItems = await collection.countDocuments(searchQuery ? query : {});
-    console.log('Number of data with MongoDB query.'); // 로그 추가: MongoDB에서 가져올 데이터 카운트
-    //const data = await collection.find(searchQuery ? query : {})
+    console.log('Number of data with MongoDB query.');
     const data = await collection.find(searchQuery ? query : {}, { projection: { dataId: 1, dataName: 1, dataTask: 1, dataUrl: 1, summary: 1, useCaseText: 1 } })
       .skip((page - 1) * itemsPerPage)
       .limit(itemsPerPage)
       .toArray();
-    console.log('Data fetched successfully'); // 로그 추가: 데이터 가져오기 성공
+    console.log('Data fetched successfully');
 
     res.json({ data, totalItems });
   } catch (err) {
     res.status(500).send('Error fetching data from MongoDB');
     console.error('Error fetching data from MongoDB:', err);
-  } finally {
-    console.log('Closing MongoDB connection'); // 로그 추가: MongoDB 연결 종료
-    await client.close();
   }
 });
 
 app.get('/image/:dataId', async (req, res) => {
   const { dataId } = req.params;
-  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
   try {
-    await client.connect();
+    // Check if the image is in the cache
+    const cachedImage = imageCache.get(dataId);
+    if (cachedImage) {
+      console.log('Serving image from cache:', dataId);
+      res.type('image/jpeg');
+      res.send(Buffer.from(cachedImage, 'base64'));
+      return;
+    }
+
+    const client = await getClient();
     const collection = client.db(dbName).collection(collectionName);
 
     const item = await collection.findOne({ dataId: dataId }, { projection: { screenShot: 1, imgSrc: 1 } });
 
     if (item && item.screenShot) {
+      // Store the image in the cache
+      imageCache.set(dataId, item.screenShot);
+
       res.type('image/jpeg');
       res.send(Buffer.from(item.screenShot, 'base64'));
     } else if (item && item.imgSrc) {
@@ -82,8 +106,6 @@ app.get('/image/:dataId', async (req, res) => {
   } catch (err) {
     res.status(500).send('Error fetching image from MongoDB');
     console.error('Error fetching image from MongoDB:', err);
-  } finally {
-    await client.close();
   }
 });
 
