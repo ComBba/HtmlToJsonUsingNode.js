2cc054126fcedabf4b8a1d8994b17cf88a4c61de
ComBba <cent@naver.com>
2023-05-15 23:32:04 +0900

    오류시 재처리 시도 관련 추가 수정.

--- tools/addFavicon.js
+++ tools/addFavicon.js
@@ -10,49 +10,56 @@ const uri = process.env.MONGODB_CONNECTION_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 (async function main() {
-    const maxRetries = 3;
-    let retryCount = 0;
+    try {
+        console.log('Adding favicon to documents started...');
+        await client.connect();
+        console.log('client.connect()...');
+        const db = client.db(process.env.MONGODB_DATABASE_NAME);
+        const collection = db.collection(process.env.MONGODB_COLLECTION_NAME);
 
-    while (retryCount < maxRetries) {
-        try {
-            console.log('Adding favicon to documents started...');
-            await client.connect();
-            console.log('client.connect()...');
-            const db = client.db(process.env.MONGODB_DATABASE_NAME);
-            const collection = db.collection(process.env.MONGODB_COLLECTION_NAME);
+        const documents = await collection
+            .find(
+                { $or: [{ favicon: { $exists: false } }, { favicon: '' }] },
+                //{dataUrl: 'https://www.nokeai.com/'},
+                //{},
+                { projection: { _id: 1, dataUrl: 1 } } // 필요한 필드만 선택
+            )//.limit(10)
+            .toArray();
+        console.info(`Found \x1b[36m${documents.length}\x1b[0m documents to update favicon.`);
 
-            const documents = await collection
-                .find(
-                    { $or: [{ favicon: { $exists: false } }, { favicon: '' }] },
-                    //{dataUrl: 'https://www.nokeai.com/'},
-                    //{},
-                    { projection: { _id: 1, dataUrl: 1 } } // 필요한 필드만 선택
-                )
-                .toArray();
-            console.info(`Found \x1b[36m${documents.length}\x1b[0m documents to update favicon.`);
+        for (const doc of documents) {
+            const { _id, dataUrl } = doc;
+            const maxRetries = 3;
+            let retryCount = 0;
+            let success = false;
 
-            for (const doc of documents) {
-                const { _id, dataUrl } = doc;
-                console.log(`[NOTICE][DOCU] Processing ${dataUrl}`);
-                const favicon = await fetchFaviconAsBase64(dataUrl);
-                if (favicon) {
-                    await collection.updateOne({ _id }, { $set: { favicon } });
-                    console.log(`[\x1b[32mOK\x1b[0m][DOCU] Updated favicon for ${dataUrl}`);
-                } else {
-                    console.log(`[\x1b[31mFail\x1b[0m][DOCU] Could not fetch favicon for ${dataUrl}`);
+            while (retryCount < maxRetries && !success) {
+                try {
+                    console.log(`[NOTICE][DOCU] Processing ${dataUrl}`);
+                    const favicon = await fetchFaviconAsBase64(dataUrl);
+                    if (favicon) {
+                        await collection.updateOne({ _id }, { $set: { favicon } });
+                        console.log(`[\x1b[32mOK\x1b[0m][DOCU] Updated favicon for ${dataUrl}`);
+                        success = true;
+                    } else {
+                        console.log(`[\x1b[31mFail\x1b[0m][DOCU] Could not fetch favicon for ${dataUrl}`);
+                        retryCount++;
+                    }
+                } catch (error) {
+                    console.error(`[Warn][DOCU]Error adding favicon to document ${dataUrl}:`, error);
+                    retryCount++;
+                    if (retryCount < maxRetries) {
+                        await new Promise((resolve) => setTimeout(resolve, 5000)); // 5초 지연 후 재시도
+                    }
                 }
-                await new Promise((resolve) => setTimeout(resolve, 1000)); // 1초 지연
             }
-        } catch (error) {
-            console.error('[Warn][DOCU]Error adding favicon to documents:', error);
-            retryCount++;
-
-            if (retryCount < maxRetries) {
-                await new Promise((resolve) => setTimeout(resolve, 5000)); // 5초 지연 후 재시도
-            }
-        } finally {
-            await client.close();
+            await new Promise((resolve) => setTimeout(resolve, 1000)); // 1초 지연
         }
+    } catch (error) {
+        console.error('[Warn][DOCU]Error adding favicon to documents:', error);
+    } finally {
+        await client.close();
     }
+
     console.log('Adding favicon to documents completed.');
 })();
